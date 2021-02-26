2063a110ab419dc3764a4572b5e169733c39a991
walisa2204 <walterunellez@gmail.com>
2021-02-25 23:37:49 -0500

    master

---
+++ taller1_php_25_02_2021.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+/*
+1.	Programa que lea un valor de precio base y calcule su impuesto, si es 
+valor es mayor de 150.000 se le aplicará un 19% de impuestos, en caso 
+contrario se le aplicará un 16%.
+*/
+
+
+
+?>
